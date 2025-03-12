25351f14ceaaa4ea75a0a06d5806b08d8f65f55d
PokeGuys <5060799+PokeGuys@users.noreply.github.com>
2025-03-13 01:31:32 +0800

    fix: missing postprocessing script

---
+++ cmd/tools/fbsprocessor/go.mod
@@ -0,0 +1,7 @@
+module github.com/arisu-archive/arona-flatbuffers/cmd/tools/fbsprocessor
+
+go 1.22.5
+
+require github.com/bmatcuk/doublestar/v4 v4.8.1
+
+replace github.com/arisu-archive/arona-flatbuffers => ../../..
---
+++ cmd/tools/fbsprocessor/internal/fbstools/options.go
@@ -0,0 +1,17 @@
+package fbstools
+
+import "log/slog"
+
+type ProcessorOptions struct {
+	Directory string
+	Language  Language
+	DryRun    bool
+}
+
+type ProcessorOption func(*Processor)
+
+func WithLogger(logger *slog.Logger) ProcessorOption {
+	return func(p *Processor) {
+		p.logger = logger
+	}
+}
